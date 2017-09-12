8c549de2244b783b13a4b9fdad73c19a985d5709
Ethan M <emohan@luc.edu>
2017-09-12 02:20:50 +0000

    lab1-part3

---
+++ src/main/java/hw/fizzbuzz3.java
@@ -0,0 +1,45 @@
+//package hw;
+import java.util.Scanner;
+public class fizzbuzz3 {
+ public static void main(String[] args) {
+     
+
+  {
+   System.out.println("input a number to determine fizzbuzz and have it entered into an array: ");
+   Scanner sc = new Scanner(System.in);
+   int n = sc.nextInt();
+   for (int i = 1; i <= n; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+     System.out.println("fizzbuzz");
+    } else if (i % 5 == 0) {
+     System.out.println("buzz");
+    } else if (i % 3 == 0) {
+     System.out.println("fizz");
+    } else {
+     System.out.println(i);
+    }
+   }
+  }
+ }
+
+ public static String[] fizzbuzz(int n) {
+  if (n >= 1) {
+   String[] testArray = new String[n];
+   for (int i = 1; i <= n; i++) {
+    if ((i % 15) == 0) {
+     testArray[i] = "fizz buzz";
+    } else if (i % 5 == 0) {
+     testArray[i] = "buzz";
+    } else if (i % 3 == 0) {
+     testArray[i] = "fizz";
+    } else {
+     testArray[i] = String.valueOf(i);
+    }
+
+   }
+
+   return testArray;
+  }
+  return null;
+ }
+}
