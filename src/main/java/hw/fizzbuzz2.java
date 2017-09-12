fe1854717416022769ade31e76f0890757699e6d
Eththenet <ethannmohan@gmail.com>
2017-09-11 23:15:28 -0500

    lab1-part2

--- src/main/java/hw/fizzbuzz2.java
+++ src/main/java/hw/fizzbuzz2.java
@@ -1,26 +1,27 @@
 
+import java.util.Scanner;
 
-   import java.util.Scanner;
-
-   public class fizzbuzz2 {
+public class fizzbuzz2 {
 
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner keyboard = new Scanner(System.in);
-    System.out.println("Please enter a number and I will make a Fizz buzz sequence for you");
-    int n = keyboard.nextInt();
-    int i;
-    for (i = 0; i < n; i++) {
-     if (i % 15 == 0) {
-      System.out.println("fizzbuzz");
-     } else if ((i % 5) == 0) {
-      System.out.println("buzz");
-     } else if ((i % 3) == 0) {
-      System.out.println("fizz");
-     } else {
-      System.out.println(i);
-     }
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter a number and I will make a Fizz buzz sequence for you");
+        int n = keyboard.nextInt();
+        int i;
+        for (i = 0; i < n; i++) {
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if ((i % 5) == 0) {
+                System.out.println("buzz");
+            } else if ((i % 3) == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
-   }
-  }
+}
+
+  
